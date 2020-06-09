dbca07430ba595131d2971009c37004f48d5e9a3
Cristian Iacob <ici.faq@gmail.com>
2020-06-09 21:34:10 +0300

    style personal info

--- components/Group/index.jsx
+++ components/Group/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Group(props) {
   return (
-    <main className={`w-full flex flex-col ${props.title ? "mb-4 " : ""}items-start rounded-sm px-4 py-4 sm:mb-1 sm:shadow`}>
+    <main className={`w-full flex flex-col ${props.title ? "mb-4 " : ""}items-start rounded-sm px-5 py-5 sm:mb-1 sm:shadow`}>
       {props.title && <h1 className="text-xl font-bold mb-6 pb-1 border-gray-400 border-b w-full">{props.title}</h1>}
       {props.children}
     </main>
--- components/PersonalInfo/index.jsx
+++ components/PersonalInfo/index.jsx
@@ -10,12 +10,12 @@ import Group from "../Group";
 function PersonalInfo(props) {
   const { name, title, workplace } = props.data;
   return (
-    <Group title="">
-      <section className="w-full flex items-center py-4 md:flex-row lg:flex-row">
-        <img className="w-24 h-24 md:w-36 md:h-36 lg:w-36 lg:h-36 object-cover object-center rounded-full" src={`/profile.jpg`} />
-        <section className="flex flex-col ml-8 items-center md:items-start md:ml-12 font-serif md:mt-0">
-          <h1 className="text-2xl font-sans font-medium">{name}</h1>
-          <span className="text-base font-sans text-blue-700">{title}</span>
+    <Group>
+      <section className="w-full mt-4 flex flex-col items-center md:flex-row">
+        <img className="w-32 h-32 shadow-lg md:w-36 md:h-36 lg:w-36 lg:h-36 object-cover object-center rounded-full" src={`/profile.jpg`} />
+        <section className="flex flex-col mt-3 items-center md:items-start md:ml-12 font-serif md:mt-0">
+          <h1 className="text-2xl font-sans font-bold">{name}</h1>
+          <span className="text-base font-sans font-medium text-blue-700">{title}</span>
         </section>
       </section>
     </Group>
