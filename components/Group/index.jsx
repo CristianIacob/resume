1b49fa434820d17e82992ba8efc0bf5429fd1309
Cristian Iacob <ici.faq@gmail.com>
2020-06-15 00:19:11 +0300

    Fix alignment issue and circle size.

--- components/Group/index.jsx
+++ components/Group/index.jsx
@@ -5,7 +5,7 @@ function Group(props) {
     <main className="w-full flex flex-col items-start py-10">
       <div className="flex items-center justify-start w-full">
         <span className="h-10 flex items-center">
-          {props.icon && <span className="w-12 h-10 p-2 fill-current rounded-full bg-blue-600 text-white mr-3">{props.icon} </span>}
+          {props.icon && <span className="w-12 h-11 p-2 fill-current rounded-full bg-blue-600 text-white mr-3">{props.icon} </span>}
           {props.title && (
             <h1 className="text-2xl font-serif text-blue-600 font-bold pb-1 w-full tracking-wider lg:text-xl">{props.title}</h1>
           )}
