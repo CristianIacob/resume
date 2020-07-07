0b84d6cf39abbb5f74d615a27201c9fb89d077ea
Cristian Iacob <ici.faq@gmail.com>
2020-07-07 21:19:22 +0300

    Improve skill chart.

--- components/Skills/chart.jsx
+++ components/Skills/chart.jsx
@@ -1,44 +1,51 @@
 import React, { useRef, useEffect } from "react";
 import Chart from "chart.js";
 
+const config = {
+  type: "doughnut",
+  data: {
+    labels: ["Frontend", "Backend", "Devops"],
+    datasets: [
+      {
+        backgroundColor: ["#CBD5E0", "#E2E8F0", "#EDF2F7"],
+        borderColor: ["#FFF"],
+        data: [70, 20, 10],
+      },
+    ],
+    options: {
+      maintainAspectRatio: false,
+      plugins: {
+        datalabels: {
+          formatter: (value, ctx) => {
+            let sum = 0;
+            let dataArr = ctx.chart.data.datasets[0].data;
+            dataArr.map((data) => {
+              sum += data;
+            });
+            let percentage = ((value * 100) / sum).toFixed(2) + "%";
+            return percentage;
+          },
+          color: "#fff",
+        },
+      },
+      title: {
+        display: true,
+        text: "Custom Chart Title",
+      },
+    },
+  },
+};
+
 function TechChart() {
   let chartRef = useRef();
 
   useEffect(() => {
     if (chartRef) {
-      chartRef = new Chart(chartRef.current, {
-        type: "doughnut",
-        data: {
-          labels: ["Frontend", "Backend", "Devops"],
-          datasets: [
-            {
-              label: "My First dataset",
-              backgroundColor: ["#CBD5E0", "#E2E8F0", "#EDF2F7"],
-              borderColor: ["#FFF"],
-              data: [70, 20, 10],
-            },
-          ],
-          options: {
-            responsive: false,
-            layout: {
-              padding: {
-                left: 0,
-                right: 0,
-                top: 50,
-                bottom: 0,
-              },
-            },
-            title: {
-              display: true,
-              text: "Custom Chart Title",
-            },
-          },
-        },
-      });
+      chartRef = new Chart(chartRef.current, config);
     }
   });
   return (
-    <div className="relative w-full h-24">
+    <div style={{ position: "relative", maxWidth: "320px", height: "160px" }} className="mt-1">
       <canvas ref={chartRef} />
     </div>
   );
