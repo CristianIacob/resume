0b51389cc5d66356b8b0cf69fb6871e003e9120e
Cristian Iacob <ici.faq@gmail.com>
2020-07-11 19:12:48 +0300

    Add next config with uglifyjs.

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const Uglify = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  webpack: function (c) {
+    c.plugins = c.plugins.filter((plugin) => plugin.constructor.name !== "UglifyJsPlugin");
+
+    c.plugins.push(new Uglify());
+
+    return c;
+  },
+};
