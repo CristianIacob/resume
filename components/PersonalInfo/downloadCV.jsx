d4f66751757c220e250edb5b3f123d61e6104e44
Cristian Iacob <ici.faq@gmail.com>
2020-07-14 21:09:23 +0300

    Fix download resume styling.

--- components/PersonalInfo/downloadCV.jsx
+++ components/PersonalInfo/downloadCV.jsx
@@ -17,7 +17,7 @@ function DownloadCV() {
 
   return (
     <button
-      className="my-5 w-5/6 text-gray-700 shadow hover:shadow-md focus:outline-none font-bold py-2 px-4 rounded-lg capitalize flex items-center font-sans tracking-wider text-xs"
+      className="my-5 w-4/5 text-gray-700 shadow hover:shadow-md focus:outline-none font-bold py-2 px-4 rounded-lg capitalize flex items-center font-sans text-xs"
       onClick={handleDownloadCV}>
       <span className="w-3 h-3 mr-3 text-gray-700 fill-current">{download}</span>download resume
     </button>
