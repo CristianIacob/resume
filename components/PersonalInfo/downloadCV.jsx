1626f6808f2beccbf0c8c090514951d024de3244
Cristian Iacob <ici.faq@gmail.com>
2020-08-12 19:05:01 +0300

    Update PDF export name and file.

--- components/PersonalInfo/downloadCV.jsx
+++ components/PersonalInfo/downloadCV.jsx
@@ -4,7 +4,7 @@ import { download } from "../../Assets/svg/dynamic-svg";
 function DownloadCV() {
   return (
     <a
-      href="cv_export.pdf"
+      href="cristian_iacob_resume.pdf"
       download
       className="my-5 text-gray-700 shadow hover:shadow-md hover:no-underline focus:outline-none font-bold py-2 px-4 rounded-lg capitalize flex items-center font-sans text-xs">
       <span className="w-3 h-3 mr-3 text-gray-700 fill-current">{download}</span>download resume
