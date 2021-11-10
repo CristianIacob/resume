d6aaff25b379c0b8a45b8dd236776872f789f78a
Cristian Iacob <ici.faq@gmail.com>
2021-11-10 22:00:21 +0200

    Update packages.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
-    return config;
-  },
-  webpack5: false,
-};
+module.exports = {};
